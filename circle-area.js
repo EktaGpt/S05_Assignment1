7705d8b0f6dbfdfe851828e66aff1defd895a0e1
EktaGpt <ekta.gpt@gmail.com>
2016-02-09 22:25:26 +0530

    Circle Area using javascript

---
+++ circle-area.js
@@ -0,0 +1,8 @@
+
+  function calcCircleArea(r){
+      
+        return Math.PI * r * r; 
+}
+document.write('r = 7; area = ' + calcCircleArea(7) + "<br />");
+document.write('r = 1.5; area = ' + calcCircleArea(1.5) + "<br />");
+document.write('r = 20; area = ' + calcCircleArea(20) + "<br />");
